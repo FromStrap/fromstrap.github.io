ddfde58ed3a8bd70a852320e470de2a7ee86548b
Washington Junior <washingtonj@outlook.com.br>
2020-09-03 02:13:13 -0300

    Adicionado menu de contato e botao de contato em telas menores que 768px.

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// Contact menu
+
+const $ = document.querySelector.bind(document);
+
+function isActive() {
+    let isActive = $("aside")    
+    !isActive.style.left || isActive.style.left == "100%" 
+        ? isActive.style.left = "0"
+        : isActive.style.left = "100%"
+
+}
+
+$(".navbar-menu li:last-child").addEventListener("click", isActive);
+$("aside h3").addEventListener("click", isActive)
+
