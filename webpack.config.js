f2174ebcf0c9b37a9d9dcefb733792d4a2decf30
Washington Junior <washingtonj@outlook.com.br>
2020-09-03 17:13:03 -0300

    Adicionando Webpack ao projeto.
    Necessário para gerar bundle dos arquivos diminuindo seus tamanhos.
    Necessário para auxiliar em futuras pipelinas de CI com o GitHub Pages.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './assets/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new OptimizeCSSAssetsPlugin({}), new TerserPlugin({})]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            publicPath: 'assets',
+                            outputPath: 'assets'
+                        },
+                    }
+                ]
+            }
+        ]
+    }
+}
