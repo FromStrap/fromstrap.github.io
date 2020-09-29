e7c06a16f88d393f172013c1aa8751d82b4450fa
Washington Junior <washingtonj@Washingtons-MacBook-Pro.local>
2020-09-29 19:55:35 -0300

    Correção do formulário (Temporario)
    O formulário agora envia uma mensagem via WhatsApp com as informações digitadas no formulário.
    
    O fomulário agora tem filtros basicos feitos pelo HTML.

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,28 @@ function isActive() {
 $(".navbar-menu li:last-child").addEventListener("click", isActive);
 $("aside i").addEventListener("click", isActive)
 
+
+function sendMessage(event) {
+    event.preventDefault();
+    
+    let inputs = $$('.contact__input')
+    let values = {}
+
+    inputs.forEach(input => {
+        let name = input.name
+        let value = input.value
+        values[name] = value
+    })
+
+    
+    const message = `"${values.msg}". Sou ${values.name}, meus meios de contato são pelo telefone ${values.phone} ou pelo email ${values.mail}.`
+    const to = "5511960819339"
+    const api = "wa.me"
+
+    window.open(`https://${api}/${to}?text=${message}`)
+
+
+}
+
+$('.contact__send').addEventListener("submit", sendMessage)
+
